0af575632fc045e40d2455916dd55fb534168802
anilkumarmeena <anil98meena@gmai.com>
2019-02-21 19:14:41 +0530

    post

--- api/views.py
+++ api/views.py
@@ -9,12 +9,9 @@ class Listque(viewsets.ModelViewSet):
     queryset = Questions.objects.all()
     serializer_class = queSerializer
 
-
     def post(self, request):
-        article = request.data.get('article')
-
         # Create an article from the above data
-        serializer = queSerializer(data=article)
+        serializer = queSerializer(data=request.data)
         if serializer.is_valid(raise_exception=True):
             article_saved = serializer.save()
-        return Response({"success": "Article '{}' created successfully".format(article_saved.title)})
+        return Response({"success": "'{}' created successfully".format(article_saved.title)})
