73b99c3fb6760d2174994c34705c1574c99c5944
anilkumarmeena <anil98meena@gmail.com>
2019-03-15 12:09:35 +0530

    frontend

--- Bitora/settings.py
+++ Bitora/settings.py
@@ -78,8 +78,11 @@ WSGI_APPLICATION = 'Bitora.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'bitora',
+        'USER': 'root',
+        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
     }
 }
 
