58be92e230758bea8b654b8294f80d4c8f6778a1
anilkumarmeena <anil98meena@gmail.com>
2020-03-21 11:05:41 +0530

    project completed

--- api/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-
-from .models import Question, Answers
-
-admin.site.register(Question)
-admin.site.register(Answers)
--- frontend/src/app/answers/answers.component.ts
+++
@@ -1,7 +0,0 @@
-import {Component} from '@angular/core';
-
-@Component({
-  selector: 'app-answers',
-  templateUrl: './answers.component.html'
-})
-export class AnswersComponent { }
